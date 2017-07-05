919943e0da4eb506303943f5b72f95020d0bf764
Rockwell Schrock <schrockwell@gmail.com>
2017-07-05 10:04:33 -0400

    Bumping to v2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bodyguard.Mixfile do
   def project do
     [
       app: :bodyguard,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
