3d9f53b6d7a8447948904eed7e8f8ed1a74e4ff8
Rockwell Schrock <schrockwell@gmail.com>
2021-02-16 14:49:46 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bodyguard.Mixfile do
   def project do
     [
       app: :bodyguard,
-      version: "2.4.0",
+      version: "2.4.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
