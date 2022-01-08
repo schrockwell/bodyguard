8dc707e49e2630996d1e8ae2b2bde3fd7879e2f9
Rockwell Schrock <schrockwell@gmail.com>
2022-01-07 20:12:55 -0500

    Re-add policy module name warning

--- lib/bodyguard/generators/policy_generator.ex
+++ lib/bodyguard/generators/policy_generator.ex
@@ -7,6 +7,8 @@ defmodule Bodyguard.Generators.PolicyGenerator do
 
   def init(args) do
     with {:ok, parsed, [policy_name]} <- parse_switches(args, [app: :string]) do
+      warn_name(policy_name)
+
       {:ok, %{policy: String.to_atom("Elixir.#{policy_name}"), switches: parsed}}
     end
   end
@@ -16,5 +18,11 @@ defmodule Bodyguard.Generators.PolicyGenerator do
     path = module_to_path(switches[:app], policy)
 
     [%{content: content, path: path}]
+  end
+
+  defp warn_name(policy_name) do
+    unless String.ends_with?(policy_name, "Policy") do
+      IO.warn("Policy modules should have the 'Policy' suffix, e.g. '#{policy_name}Policy'", [])
+    end
   end
 end
